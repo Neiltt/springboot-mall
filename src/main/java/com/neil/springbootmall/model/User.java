2dac219616a501fd45677f341885ec9fdb16abe2
Neil Tu <de213049.th@gmail.com>
2024-07-19 17:21:18 +0800

    帳號功能 - 檢查註冊email、隱藏返回的密碼

--- src/main/java/com/neil/springbootmall/model/User.java
+++ src/main/java/com/neil/springbootmall/model/User.java
@@ -1,12 +1,20 @@
 package com.neil.springbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 
 public class User {
 
     private Integer userId;
+
     private String email;
+
+    @JsonIgnore
     private String password;
+
     private Date createdDate;
     private Date lastModifiedDate;
 
